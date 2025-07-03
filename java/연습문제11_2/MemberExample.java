133ec9c409ee1968c37b96645ec3a509e42dd719
rlagudwnsdla <jhk032200@gmail.com>
2025-07-03 20:52:34 +0900

    Add files via upload

---
+++ java/연습문제11_2/MemberExample.java
@@ -0,0 +1,8 @@
+package two;
+
+public class MemberExample {
+    public static void main(String[] args) {
+        Member member = new Member("Blue","아파란");
+        System.out.println(member);
+    }
+}
